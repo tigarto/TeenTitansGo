73cef4c37bd7f18de91c1a6057d4d9b71c27d236
tigarto <helpu1409@gmail.com>
2018-04-26 17:12:12 -0500

    Agregando formula del bachiller. No se por que pero parece que esto jode mucho con tipos de datos de distinto tipo.

--- ejemplos1/sol_problema5.go
+++ ejemplos1/sol_problema5.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+  "math"
 )
 
 
@@ -25,18 +26,40 @@ func test_discriminante()  {
   fmt.Printf("%d\n",w)
 }
 
-func obtener_raices(a, b, c int) string {
-  D := discriminante(a, b, c)
+func obtener_raices(a, b, c int) {
+  var D float64 = float64(discriminante(a, b, c))
   if D > 0 {
     // Raices reales y distintas
+    // x1 = (-b - D^(1/2))/(2*a)
+    var x1 float64 = (float64(-b) - math.Sqrt(D))/float64(2*a)
+    //x2 = (-b + D^(1/2))/(2*a)
+    //var x2 float64 = (float64(-b) + math.Sqrt(D))/float64(2*a)
+    fmt.Printf("%.1f con multiplicidad 2.\n",x1)
   } else if D == 0 {
     // Raices reales e iguales
+    // x1 = x2 = -b/(2*a)
+    var x1 float64 = float64(-b/(2*a))
+    var x2 float64 = float64(-b/(2*a))
+    fmt.Printf("%.1f, %.1f\n",x1, x2)
   } else {
     // Raices complejas conjugadas
+    // x1 = (-b/(2*a))i - ((-D)^(1/2))/(2*a))j
+    var pR float64 = float64(-b/(2*a))
+    var pI float64 = math.Sqrt(-D)/float64(2*a)
+    if pR != 0 {
+      if pI > 0 {
+        fmt.Printf("%.1f + %.1fj, %.1f - %.1fj\n",pR, pI,pR,pI)
+      } else if pI < 0 {
+        fmt.Printf("%.1f + %.1fj, %.1f - %.1fj\n",pR, -pI, pR,-pI)
+      }
+    } else {
+      if pI > 0 {
+        fmt.Printf("%.1fj, -%.1fj\n",pI, pI)
+      } else {
+        fmt.Printf("%.1fj, -%.1fj\n",-pI, -pI)
+      }
+    }
   }
-
-
-
 }
 
 
