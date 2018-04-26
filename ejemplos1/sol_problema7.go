31bcd3b7e7411ed5d156c00a6f151b64acea5a39
tigarto <helpu1409@gmail.com>
2018-04-26 18:04:53 -0500

    Se agrego multiples ejecuciones

--- ejemplos1/sol_problema7.go
+++ ejemplos1/sol_problema7.go
@@ -28,20 +28,25 @@ func lanzarMondena(numL int)  {
 
 func main() {
   rand.Seed(time.Now().Unix())
-  num1 := generar_digito()
-  num2 := generar_digito()
-  var resp int
-  var ban bool = false
-  fmt.Printf("¿Cuantas %d veces %d? -> ",num1,num2);
-  fmt.Scanln(&resp)
-  for ban == false {
-    if resp == num1*num2 {
-      fmt.Printf("Muy bien!\n")
-      ban = true
-    } else {
-      fmt.Printf("No. Por favor intenta nuevamente\n")
-      fmt.Printf("¿Cuantas %d veces %d? -> ",num1,num2);
-      fmt.Scanln(&resp)
+  var salir string = "n"
+  for salir == "n" {
+    num1 := generar_digito()
+    num2 := generar_digito()
+    var resp int
+    var ban bool = false
+    fmt.Printf("¿Cuantas %d veces %d? -> ",num1,num2);
+    fmt.Scanln(&resp)
+    for ban == false {
+      if resp == num1*num2 {
+        fmt.Printf("Muy bien!\n")
+        ban = true
+      } else {
+        fmt.Printf("No. Por favor intenta nuevamente\n")
+        fmt.Printf("¿Cuantas %d veces %d? -> ",num1,num2);
+        fmt.Scanln(&resp)
+      }
     }
+    fmt.Printf("¿Desea salir (y/n)? ");
+    fmt.Scanln(&salir)
   }
 }
