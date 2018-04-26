b3752f1d2f8cf6320ae154783712b526d4bd514c
tigarto <helpu1409@gmail.com>
2018-04-26 17:32:32 -0500

    ejercicio completo

--- ejemplos1/sol_problema5.go
+++ ejemplos1/sol_problema5.go
@@ -5,47 +5,25 @@ import (
   "math"
 )
 
-
-
-func discriminante(a, b, c int) int  {
+func discriminante(a, b, c float64) float64  {
   return b*b - 4*a*c
 }
 
-
-func test_discriminante()  {
-  x := 1
-  y := 2
-  z := 6
-  w := 0
-  w = discriminante(1,0,1)
-  fmt.Printf("%d\n",w)
-  w = discriminante(y,z,x)
-  fmt.Printf("%d\n",w)
-  z = z - 1
-  w = discriminante(y,z,5)
-  fmt.Printf("%d\n",w)
-}
-
-func obtener_raices(a, b, c int) {
-  var D float64 = float64(discriminante(a, b, c))
+func obtener_raices(a, b, c float64) {
+  var D float64 = discriminante(a, b, c)
   if D > 0 {
     // Raices reales y distintas
-    // x1 = (-b - D^(1/2))/(2*a)
-    var x1 float64 = (float64(-b) - math.Sqrt(D))/float64(2*a)
-    //x2 = (-b + D^(1/2))/(2*a)
-    //var x2 float64 = (float64(-b) + math.Sqrt(D))/float64(2*a)
-    fmt.Printf("%.1f con multiplicidad 2.\n",x1)
+    var x1 float64 = ((-b) - math.Sqrt(D))/(2*a)
+    var x2 float64 = ((-b) + math.Sqrt(D))/(2*a)
+    fmt.Printf("%.1f, %.1f\n",x1, x2)
   } else if D == 0 {
     // Raices reales e iguales
-    // x1 = x2 = -b/(2*a)
-    var x1 float64 = float64(-b/(2*a))
-    var x2 float64 = float64(-b/(2*a))
-    fmt.Printf("%.1f, %.1f\n",x1, x2)
+    var x float64 = -b/(2*a)
+    fmt.Printf("%.1f con multiplicidad 2.\n",x)
   } else {
     // Raices complejas conjugadas
-    // x1 = (-b/(2*a))i - ((-D)^(1/2))/(2*a))j
-    var pR float64 = float64(-b/(2*a))
-    var pI float64 = math.Sqrt(-D)/float64(2*a)
+    var pR float64 = -b/(2*a)
+    var pI float64 = math.Sqrt(-D)/(2*a)
     if pR != 0 {
       if pI > 0 {
         fmt.Printf("%.1f + %.1fj, %.1f - %.1fj\n",pR, pI,pR,pI)
@@ -62,7 +40,59 @@ func obtener_raices(a, b, c int) {
   }
 }
 
+func test_discriminante()  {
+  var x float64 = 1
+  var y float64 = 2
+  var z float64 = 6
+  var w float64 = 0
+  w = discriminante(1,0,1)
+  fmt.Printf("%d\n",w)
+  w = discriminante(y,z,x)
+  fmt.Printf("%d\n",w)
+  z = z - 1
+  w = discriminante(y,z,5)
+  fmt.Printf("%d\n",w)
+}
+
+func test_bachiller()  {
+  var a float64 = 1
+  var b float64 = 2
+  var c float64 = 2
+  fmt.Printf("(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+  a = 1
+  b = 3
+  c = 2
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+  a = 1
+  b = 2
+  c = 1
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+  a = 2
+  b = 3
+  c = 1
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+  a = 2.1
+  b = 3.2
+  c = 1.7
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+  a = 2
+  b = 2
+  c = 0.5
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  obtener_raices(a,b,c)
+}
 
 func main() {
-  test_discriminante()
+  var a float64
+  var b float64
+  var c float64
+  fmt.Printf("\n(%.1f)x^2 + (%.1f)*x + (%.1f) = 0\n",a , b, c)
+  fmt.Printf("\nRaices de la ecuacion ->\n")
+  obtener_raices(a, b, c)
+
 }
